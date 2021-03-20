4d9f4a1bb40b234f86b8da2370a0da3d0f413e9b
batshoes <james.middlemiss@valiant.finance>
2021-03-20 20:34:33 +1100

    Revert

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,58 @@
-document.addEventListener(
-    'click',
-    function (event) {
-        // Only fire if the target has id copy
-        if (!event.target.matches('#copy')) return;
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-        if (!navigator.clipboard) {
-            // Clipboard API not available
-            return;
-        }
-        var text = event.target.innerText;
-        try {
-            navigator.clipboard.writeText(text);
-            document.getElementById('copy').innerText = 'Copied to clipboard!';
-            setTimeout(function () {
-                document.getElementById('copy').innerText = 'james.o.middlemiss@gmail.com';
-            }, 1200);
-        } catch (err) {
-            console.error('Failed to copy!', err);
-        }
-    },
-    false
-);
+    konamify("arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter");
 
+    copyEmail();
+});
+
+function copyEmail() {
+    document.addEventListener(
+        'click',
+        function (event) {
+            // Only fire if the target has id copy
+            if (!event.target.matches('#copy')) return;
+
+            if (!navigator.clipboard) {
+                // Clipboar d API not available
+                return;
+            }
+            var text = event.target.innerText;
+            try {
+                navigator.clipboard.writeText(text);
+                document.getElementById('copy').innerText = 'Copied to clipboard!';
+                setTimeout(function () {
+                    document.getElementById('copy').innerText = 'james.o.middlemiss@gmail.com';
+                }, 1200);
+            } catch (err) {
+                console.error('Failed to copy!', err);
+            }
+        },
+        false
+    );
+
+}
+
+function konamify(code) {
+    let buffer = [];
+    let lastKeyTime = Date.now();
+
+    document.addEventListener('keydown', event => {
+        const key = event.key.toLowerCase();
+
+        const currentTime = Date.now();
+
+        if (currentTime - lastKeyTime > 1000) {
+            buffer = [];
+        }
+
+        lastKeyTime = currentTime;
+        buffer.push(key);
+        if (buffer.join('') === code) {
+            // Konamified.
+            const scroller = document.querySelector('.konamify h3');
+            scroller.style.cssText = '-moz-animation: konamify 15s linear infinite; -webkit-animation: konamify 15s linear infinite; animation: konamify 15s linear infinite;';
+            // Your Konami Code action here.
+        }
+    });
+}
