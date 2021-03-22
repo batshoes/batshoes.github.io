6d8b68551c00e22187470d8756a4da69c29a7acb
batshoes <james.middlemiss@valiant.finance>
2021-03-22 14:59:52 +1100

    Add tooltips for highlights

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,8 +5,89 @@ document.addEventListener('DOMContentLoaded', () => {
     copyEmail();
 
     tippy('.job-description', {
-        content: 'Hello - Light',
-        theme: 'light',
+        content: 'While Ruby on Rails and Vue.js are my fav\'s. I\'m into learning more.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.interest-learning', {
+        content: 'Currently re-doing my roof. Ask me how it\'s going!',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.interest-gaming', {
+        content: 'I\'m playing Age of Calamity at the moment. I have a lot of strong opinions about how it melds perfectly with BotW.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.interest-writing', {
+        content: 'Peg Leg Meg is my daughters plushy puppy with a toilet roll peg leg. She has adventures in shopping trolley ships, cast away islands on the rug in our living room, or stranded at sea when we left her in the car overnight.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.interest-books', {
+        content: 'Paul Kalanithi\'s book \'When breath becomes air\' is the latest book I am obsessed with. It really struck me that he sort of \'fell\' into Neurosurgery by following his desire to capture knowledge about where technology intersects spirituality. Again, I have a lot of thoughts on the topic. I\'d love to chat about it.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.interest-food', {
+        content: '<a href="https://www.notion.so/batshoes/a2b1dd0ef1f1403f890c733f8a186cb1?v=254fdc1af9624cd58c6911dfee0537d9">This</a> is the \'best of\' from my time in NYC. I am in the process of creating another for Sydney.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+        interactive: true,
+    })
+
+    tippy('.contact-build', {
+        content: 'I love a side hustle',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.contact-highscore', {
+        content: 'There was a time I could run the Luigi Raceway in Mario Kart 64 Blindfolded in under 2.5 minutes',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.contact-story', {
+        content: 'Does your child also like to bring their favourite stuffed animal to spaghetti dinners? I have a cautionary tale that will keep even the most beloved fluffy animal at arms length from the saucy noodle monsters of Ta-Bel.',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.contact-recommendations', {
+        content: '<a href = "https://www.bistecca.com.au/" class="highlight" > Bistecca</a>',
+        theme: 'light-border',
+        arrow: true,
+        arrowType: 'round', // or 'sharp' (default)
+        animation: 'fade',
+    })
+
+    tippy('.contact-chat', {
+        content: 'If you have news about Breath of the Wild 2, I <em>need</em> to speak with you',
+        theme: 'light-border',
         arrow: true,
         arrowType: 'round', // or 'sharp' (default)
         animation: 'fade',
